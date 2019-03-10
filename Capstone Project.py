edb78900dd956b40e290b8371ad28a7e6dc1b48d
Anupreet21 <48406313+Anupreet21@users.noreply.github.com>
2019-03-10 19:41:34 +0530

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# # Capstone Project
+
+# *The notebook has been created as a part of assignment given under the course 'Introduction to capstone project'*
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+x="Hello Capstone Project Course!"
+
+
+# In[3]:
+
+
+print(x)
+
